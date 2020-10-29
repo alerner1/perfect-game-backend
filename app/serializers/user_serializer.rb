d500985f2cad90377388d6c3c78aa8b7f033b7f7
alerner1 <alyssplerner@gmail.com>
2020-10-28 23:10:06 -0400

    nightly

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :steam_name, :steam_id, :owned_games, :recced_games, :wishlist_games
+  has_many :user_played_games
   has_many :played_games
 
 end
